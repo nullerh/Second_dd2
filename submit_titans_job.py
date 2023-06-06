41a1bea27e72eb84d60beb5ef293a3c8154031cb
nullerh <80833969+nullerh@users.noreply.github.com>
2023-06-06 14:49:32 +0200

    Mros fit

--- submit_titans_job.py
+++ submit_titans_job.py
@@ -4,13 +4,13 @@ import tempfile
 # fmt: off
 JOBS = [
     {
-        "jobname": "DOSED",
+        "jobname": "dd2",
         "partition": "titans",
-        "reservation": "comp-gpu04",  # This is my GPU node, comment this line and remove line 27, if you wish to send the job out to all nodes
+        "reservation": "comp-gpu13",  # This is my GPU node, comment this line and remove line 27, if you wish to send the job out to all nodes
         "time": "4-00:00:00",  # Days-Hours:Minutes:Seconds
         "ncpus": 8,  # Number of CPU cores
         "gpus": 1,  # Number of GPUs
-        "memory": "128G",  # This is total RAM, change this accordingly to use
+        "memory": "128GB",  # This is total RAM, change this accordingly to use
         "command": "python main.py",
         "log_path": "/scratch/s194277/"  # Usually this is your scratch space
 
@@ -25,6 +25,7 @@ def submit_job(jobname, partition, time, reservation, ncpus, gpus, command, memo
 #SBATCH --job-name={jobname}
 #SBATCH --time={time}
 #SBATCH -p {partition}
+#SBATCH -w {reservation}
 #SBATCH --cpus-per-task={ncpus}
 #SBATCH --gres=gpu:{gpus}
 #SBATCH --mem={memory}
